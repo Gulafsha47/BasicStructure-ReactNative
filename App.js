f8989303f1fb2abdc52d93618533dc6379c0f242
Gulafsha47 <59197571+Gulafsha47@users.noreply.github.com>
2021-08-03 16:30:45 +0800

    Flex Dimension

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
-const FixedDimensionBasics = () => {
+
+const FlexDimensionBasics = () => {
   return (
-    <View>
-      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-      <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+      <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+      <View style={{flex: 3, backgroundColor: 'steelblue'}} />
     </View>
   );
 };
-
-export default FixedDimensionBasics;
+export default FlexDimensionBasics;
