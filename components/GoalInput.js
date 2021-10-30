50afcc4b6de4dbcfd5a43a2562c8628b36203728
Gulafsha Khanam <gulafshakhanam47@gmail.com>
2021-10-30 21:30:40 +0530

    clearing and closing the modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -8,6 +8,12 @@ const GoalInput = props => {
     setEnteredGoal(enteredText);
   };
 
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoal);
+    setEnteredGoal(' '); 
+
+  };
+
   return (
     <Modal visible={props.visible} animationType="slide">
     <View style={styles.inputContainer}>
@@ -16,8 +22,9 @@ const GoalInput = props => {
         style={styles.input}
         onChangeText={goalInputHandler}
         value={enteredGoal}
-      />
-      <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+        />
+        <Button title = "Cancel"  color = "red" onPress={props.onCancel} />
+      <Button title="ADD" onPress={addGoalHandler} />
       </View>
       </Modal>
   );
